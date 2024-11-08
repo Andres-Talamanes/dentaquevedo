10634282b87e0e51164dfec6c613f4e48218c110
Andres <al20760275@ite.edu.mx>
2024-11-07 18:34:33 -0800

    modificacion

--- builds.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  // Intentar ejecutar `pnpm run build`
-  execSync('pnpm run build', { stdio: 'inherit' });
-} catch (error) {
-  // Si falla, intenta con `npm run build`
-  execSync('npm run build', { stdio: 'inherit' });
-}
